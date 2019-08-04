d281a1a7b4367759ef96ab233abab11a2f1d7e0e
Pawel Mrozikowski <mrozik.p@gmail.com>
2019-08-05 01:49:16 +0200

    BEM

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ alert(`siema, udało Ci się`);
 
 console.log('siema!');
 
+
 const name = 'Pawel';
 const age = 34;
 let Pawel = null;
 
-console.log(`Nazywam się ${name} i mam ${age}`);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const about = document.querySelector('.about__test--js');
+
+console.log(about.innerHTML);
+
+about.innerHTML = 'zmiana yo';
